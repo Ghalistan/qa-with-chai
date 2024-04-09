3b60f18af0155e0db6094cc6c2b46eae58537912
G Ghalistan <gghalistan@gmail.com>
2024-04-09 19:44:27 +0000

    feat: complete test 2 headless setup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -67,16 +67,19 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://3000-ghalistan-qawithchai-fo6wwchbvuz.ws-us110.gitpod.io/'
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
-
-
+  const browser = new Browser()
+  suiteSetup(function(done) {
+    return browser.visit('/', done)
+  })
 
   suite('Headless browser', function () {
-    // test('should have a working "site" property', function() {
-    //   assert.isNotNull(browser.site);
-    // });
+    test('should have a working "site" property', function() {
+      assert.isNotNull(browser.site);
+    });
   });
 
   suite('"Famous Italian Explorers" form', function () {
